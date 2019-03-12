e16e002f6393d82523c63a07028bd642458b9f81
Denis Zaratan Pasin <denis.pasin@autodesk.com>
2019-03-12 20:54:47 +0900

    Adding Level links

--- src/templates/Discipline.jsx
+++ src/templates/Discipline.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import styled from 'styled-components';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import DisciplineLevel from '../components/DisciplineLevel';
 import SectionHeader from '../styles/SectionHeader';
+
+const LevelContainer = styled.p`
+  display: flex;
+  justify-content: center;
+`;
+
+const LevelLink = styled.a`
+  margin: 0 0.5rem;
+  padding: 0 0.5rem;
+`;
 
 const Discipline = ({ pageContext, data }) => {
   const { discipline } = pageContext;
@@ -19,6 +30,14 @@ const Discipline = ({ pageContext, data }) => {
     <Layout>
       <SEO title={discipline} />
       <SectionHeader>{discipline}</SectionHeader>
+      <LevelContainer>
+        Go to level:{' '}
+        {disciplineData.map(({ level }) => (
+          <LevelLink href={`#level-${level}`}>
+            <span>{level}</span>
+          </LevelLink>
+        ))}
+      </LevelContainer>
       <ul>
         {disciplineData.map(level => (
           <DisciplineLevel level={level} />
